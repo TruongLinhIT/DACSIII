13816d78c9bdcaa41574c5e40ff6938de5c89940
TruongLinhIT <linhtvt.24it@vku.udn.vn>
2026-04-23 00:35:35 +0700

    Update demo page title and modify greeting name in MainActivity

--- DACSIII_FE/app/src/main/java/com/example/dacsiii/MainActivity.kt
+++ DACSIII_FE/app/src/main/java/com/example/dacsiii/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             DACSIIITheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Lalo 123",
+                        name = "Lalo 123123123",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
