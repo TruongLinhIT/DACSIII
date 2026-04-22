132f7e86da03c582c69c26555627d4bdca186117
TruongLinhIT <linhtvt.24it@vku.udn.vn>
2026-04-22 23:59:19 +0700

    Add initial project files and update MainActivity greeting

--- DACSIII_FE/app/src/main/java/com/example/dacsiii/MainActivity.kt
+++ DACSIII_FE/app/src/main/java/com/example/dacsiii/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             DACSIIITheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Linh123",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
